2ef676740791df4c0ead26b79650661eaf31cd32
Shivangi <shivangigakhar@gmail.com>
2026-03-20 20:55:41 +0530

    landing page done!

--- src/landing_page/Support/Hero.js
+++ src/landing_page/Support/Hero.js
@@ -1,7 +1,37 @@
 import React from "react";
 function Hero(){
     return(
-        <h1>This is the Hero section!</h1>
+        <section className="container-fluid" id='support'>
+
+            <div className="p-5" id="supportWrapper">
+                <h3>Support Portal</h3>
+                <a href='' style={{color:"white"}}>Track Tickets</a>
+            </div>
+           <div className="row p-3 m-3">
+        <div className="col-6 p-3">
+          <h1 className="fs-3">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <input placeholder="Eg. how do I activate F&O" />
+          <br />
+          <a href="" style={{color:"white"}}>Track account opening</a>
+          <a href="" style={{color:"white"}}>Track segment activation</a>
+          <a href="" style={{color:"white"}}>Intraday margins</a>
+          <a href="" style={{color:"white"}}>Kite user manual</a>
+        </div>
+        <div className="col-6 p-3">
+          <h1 className="fs-3">Featured</h1>
+          <ol>
+            <li>
+              <a href="" style={{color:"white"}}>Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="" style={{color:"white"}}>Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+        </section>
     );
 }
 export default Hero;
